58d42c15651c7202f9b14e4583c7659e1d83724d
Axelbau <85304438+Axelbau19@users.noreply.github.com>
2023-09-10 12:27:12 -0600

    Initial commit

---
+++ practica1/helloUpiicsa/src/helloUpiicsa.kt
@@ -0,0 +1,13 @@
+/* Bautista Rosales Alexis Axel 6NM61*/
+
+fun main() {
+    /* 1. Imprimir el mismo mensaje que imprimimos en la practica "HelloUPIICSA"*/
+    print("HelloUPIICSA!!")
+    /*2.Abajo de la linea de código de la tarea
+        a.Poner otro mensaje en consola ¿Qué pasa con los dos mensajes?*/
+    print("HelloUPIICSA x 2!!")
+    /*3.Ahora utiliza println e imprime alguna otra cosa, ¿Qué diferencia hay con print?*/
+    println("HelloUpiicsa x 3 !!")
+    print("HelloUpiicsa x 4")
+
+}
