566b467edd1fb8a4464facccf26a46fa29c292c7
Axelbau <85304438+Axelbau19@users.noreply.github.com>
2023-09-24 14:46:30 -0600

    Practica 3
    Practica 3

---
+++ practica3/src/Practica3.kt
@@ -0,0 +1,14 @@
+//Para guardar solo los atributos
+data class Punto (val x : Double , val y:Double)
+
+fun obtenerPendiente(punto1:Punto, punto2: Punto): Double {
+return (punto2.y - punto1.y)/(punto2.x - punto1.x)
+}
+
+fun main(){
+    val P1 = Punto(4.0,3.0)
+    val P2 = Punto(-3.0,-2.0)
+    val pendiente= obtenerPendiente(P1,P2)
+    print("El valor de la pediente: $pendiente")
+
+}
