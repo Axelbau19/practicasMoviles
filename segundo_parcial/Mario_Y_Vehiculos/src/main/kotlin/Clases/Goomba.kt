c861e6c6f857f6fc4a067d0797a4d5087097d29b
Axelbau <85304438+Axelbau19@users.noreply.github.com>
2023-11-22 21:43:25 -0600

    Axelbau
    2do parcial

---
+++ segundo_parcial/Mario_Y_Vehiculos/src/main/kotlin/Clases/Goomba.kt
@@ -0,0 +1,10 @@
+package Clases
+
+import Clases.Enemy
+
+class Goomba: Enemy("Goomba", 1){
+ {
+    init {
+        println("Iniciando subclase de $name")
+        }
+}
---
+++ segundo_parcial/Mario_Y_Vehiculos/src/main/kotlin/Clases/Koopa.kt
@@ -0,0 +1,17 @@
+package Clases
+
+import Clases.Enemy
+
+class Koopa:
+    Enemy("Koopa", 2){
+    override fun collision(collider: String) {
+        when(collider){
+            "Weapon" -> {
+                var state = "Shell"
+                println("El estado ahora es $state")
+            }
+            "Enemy" -> changeDirection()
+        }
+        println("Usando la colisión de la clase Enemy")
+        }
+}
